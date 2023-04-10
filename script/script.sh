4a25462347940eaae9961b2d92d07e61f3c4fc02
jorge <jorge.arevalo@cision.com>
2023-04-10 14:40:36 -0300

    adding script

--- script/script.sh
+++ script/script.sh
@@ -3,7 +3,7 @@ git show --stat=1000
 echo "Files and folders:"
 git show --stat=1000 | egrep '((.*/)*.+.tf)'
 path=$(git show --stat=1000 | egrep '((.*/)*.+.tf)')
-proj_folders=$(echo "$path" | xargs -L 1 dirname)
+proj_folders=$(echo "$path" | xargs -L 1 dirname | sort -u | tr -d . )
 json=$( echo -n "[" ; git show --stat=1000 | egrep '((.*/)*.+.tf)' | echo "$proj_folders" | sort -u | ( c=""; while read line; do echo -n "$c\"$line\""; c=","; done ) ; echo "]" )
 
 echo "carpetas" $proj_folders

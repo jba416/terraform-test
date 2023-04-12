f86c5526f474af5903b3aabaf0af50cfa8e0a958
jorge <jorge.arevalo@cision.com>
2023-04-12 11:34:57 -0300

    added script

--- script/script.sh
+++ script/script.sh
@@ -4,8 +4,8 @@ echo "Files and folders:"
 git show --stat=1000 | egrep '((.*/)*.+.tf)'
 path=$(git show --stat=1000 | egrep '((.*/)*.+.tf)')
 proj_folders=$(echo "$path" | xargs -L 1 dirname | sort -u | tr -d . )
-json=$( echo -n "[" ; git show --stat=1000 | egrep '((.*/)*.+.tf)' | echo "$proj_folders" | sort -u | ( c=""; while read line; do echo -n "$c\"$line\""; c=","; done ) ; echo "]" )
-
+#json=$( echo -n "[" ; git show --stat=1000 | egrep '((.*/)*.+.tf)' | echo "$proj_folders" | sort -u |( c=""; while read line; do echo -n "$c\"$line\""; c=","; done ); echo "]" )
+json=$(echo -n "[" ; git show --stat=1000 | egrep '((.*/)*.+.tf)' | echo "$proj_folders" | sort -u |( c=""; while read line; do echo -n "$c\"$line\""; c=","; done )|cut -c 4-; echo "]" )
 echo "carpetas" $proj_folders
 echo "json" $json
 if [ "$?" == "0" -a "$json" != "[]" ]; then
